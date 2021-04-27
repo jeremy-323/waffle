fff204676f0f290181dadfa36fe59f1ed38cfa1c
Ermia Afsharian <jeremy@323.media>
2021-04-27 16:05:16 -0700

    redirect error on install 3

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "f8023b46ccb55af27f0e3990e8e28316";
 $scopes = "read_orders,write_products,write_script_tags,read_content,write_themes";
-$redirect_uri = "https://waffle-grid-compare.herokuapp.com/generate_token.php";
+$redirect_uri = "https://wafful.herokuapp.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop ."/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
