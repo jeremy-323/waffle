7e25960538aacf1123dc8233f58b6103bd015403
Ermia Afsharian <jeremy@323.media>
2021-04-27 16:27:57 -0700

    no sql database

--- inc/connect.php
+++ inc/connect.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "shopify_app";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password,$db);
-// Check connection
-if (!$conn) {
-   die("Connection failed: " . mysqli_connect_error());
-}
-echo "<br>";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $db = "shopify_app";
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password,$db);
+// // Check connection
+// if (!$conn) {
+//    die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "<br>";
