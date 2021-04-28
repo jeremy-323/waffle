09c60bd8262115fa487bb9bc93e1c1f5ee693026
Ermia Afsharian <jeremy@323.media>
2021-04-28 10:13:36 -0700

    added database

--- inc/connect.php
+++ inc/connect.php
@@ -1,12 +1,12 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $db = "shopify_app";
-// // Create connection
-// $conn = mysqli_connect($servername, $username, $password,$db);
-// // Check connection
-// if (!$conn) {
-//    die("Connection failed: " . mysqli_connect_error());
-// }
-// echo "<br>";
+$servername = "us-cdbr-east-03.cleardb.com";
+$username = "b2ca9b9dc1e4b4";
+$password = "6c118034";
+$db = "shopify_app";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+echo "<br>";
