2cf6ac8ee5902a9cc0181438485958fe12bd83ec
Ermia Afsharian <jeremy@323.media>
2021-04-28 10:15:37 -0700

    fixed database name

--- inc/connect.php
+++ inc/connect.php
@@ -2,7 +2,7 @@
 $servername = "us-cdbr-east-03.cleardb.com";
 $username = "b2ca9b9dc1e4b4";
 $password = "6c118034";
-$db = "shopify_app";
+$db = "waffle_cred";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$db);
 // Check connection
