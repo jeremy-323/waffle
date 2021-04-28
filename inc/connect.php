806545dea92f732d1dbcbd7a2ce6f572d937b26d
Ermia Afsharian <jeremy@323.media>
2021-04-28 11:22:06 -0700

    fixed database name

--- inc/connect.php
+++ inc/connect.php
@@ -3,11 +3,7 @@ $servername = "us-cdbr-east-03.cleardb.com";
 $username = "b2ca9b9dc1e4b4";
 $password = "6c118034";
 $db = "heroku_08380f7097b9ade";
-$sql = 'SELECT * FROM example_table WHERE store_url="' . $requests['shop'] . '" LIMIT 1';
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
-echo $row['store_url'];
-echo $row['access_token'];
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$db);
 // Check connection
