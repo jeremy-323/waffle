ee9fc054b572e81ca97d5bc542c36da7ee9fa5ca
Ermia Afsharian <jeremy@323.media>
2021-07-09 15:17:08 -0700

    fix gcpr webhook database name

--- webhook-delete.php
+++ webhook-delete.php
@@ -3,6 +3,6 @@ require_once('inc/functions.php');
 require_once('inc/connect.php');
 
 $shop_url = $_POST['shop_domain'];
-$query = 'DELETE FROM example_table WHERE store_url="'.$shop_url.'";';
+$query = 'DELETE FROM `waffle_cred` WHERE store_url="'.$shop_url.'";';
 if (mysqli_query($conn, $query)) {
 }
