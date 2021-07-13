361d15752b31f4d31fd48282d7622ba552df9ee1
Ermia Afsharian <jeremy@323.media>
2021-07-13 15:25:25 -0700

    fix submit bug

--- delete-asset.php
+++ delete-asset.php
@@ -6,13 +6,11 @@ $name= $_POST['name'];
 $id = $_POST['asset'];
 $subdomain = $_POST['store'];
 
-echo $subdomain;
-
 $result="";
 $sql='SELECT * FROM example_table WHERE store_url="'.$subdomain.'" LIMIT 1';
 
 $result = mysqli_query($conn, $sql);
-var_dump($result);
+
 $row= mysqli_fetch_assoc($result);
 $token= $row['access_token'];
 
