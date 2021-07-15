dd37d7176574af8c40629712e0af08bbf5471d27
Ermia Afsharian <jeremy@323.media>
2021-07-15 12:13:41 -0700

    webhook test

--- webhooks.php
+++ webhooks.php
@@ -1,12 +1,12 @@
 <?php
-define('DOMAIN_PATH',"https://wafful.herokuapp.com/");
+
 $array=array(
     'webhook' => array(
         'topic'=>'products/create',
-        'address'=>DOMAIN_PATH."webhooks/create.php",
+        'address'=>"https://wafful.herokuapp.com/webhooks/create.php",
         'format'=>'json'        
     )
     );
-$webhooks= shopify_call($token,$shop,"admin/api/2021-07/webhooks.json",$array, 'POST');
+$webhooks= shopify_call($token, $shop, "admin/api/2021-07/webhooks.json",$array, 'POST');
 $webhooks=jason_decode($webhooks['response'],true);
- echo print_r($webhooks);
+print_r($webhooks);
